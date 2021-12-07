919c165bb159f3ba4f3df0a53993c6651876de89
aleksykrolczyk <aleksykrolczyk@yahoo.pl>
2021-12-07 10:34:44 +0100

    day 7

--- day7/main.py
+++ day7/main.py
@@ -1,13 +1,24 @@
-def part1() -> ...:
-    ...
+def part1(positions: list[int]) -> int:
+    minimum = float("inf")
+    for u in range(max(positions)):
+        dist = sum(abs(pos - u) for pos in positions)
+        if dist < minimum:
+            minimum = dist
+    return minimum
 
-
-def part2() -> ...:
-    ...
+def part2(positions: list[int]) -> int:
+    minimum = float("inf")
+    f_dist = lambda d: int(d*(d+1)/2)
+    for u in range(max(positions)):
+        dist = sum(f_dist(abs(pos - u)) for pos in positions)
+        if dist < minimum:
+            minimum = dist
+    return minimum
 
 
 with open('input', 'r') as file:
-    ...
+    line = file.readline()
+    positions = [int(x) for x in line.split(',')]
 
-# print(f'{part1()=}')
-# print(f'{part2()=}')
+print(f'{part1(positions)=}')
+print(f'{part2(positions)=}')
